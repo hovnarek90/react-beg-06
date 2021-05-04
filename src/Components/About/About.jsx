90c3a2a1cdb15604f71a0dfd54ab03c6daa8a1ba
Narek Hovhannisyan <narekhovhannisyan@Nareks-MacBook-Pro.local>
2021-05-05 00:09:55 +0400

    main

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,5 +1,5 @@
 import styles from "./about.module.css";
-import Zoom from 'react-reveal/Roll';
+
 
 
 
@@ -8,9 +8,19 @@ const About = () => {
     <div className={styles.div1}>
       <div className={styles.div2}>
         <h1>
-        HELLO!
+        About me
         </h1>
-         
+        <p> Name  Narek  
+          <br/> 
+          Surname  Hovhannisyan
+          <br/>
+          E-mail hovnarek@gmail.com
+
+           
+        </p>
+        <h2>I am an allround web developer. I am a programmer with good knowledge of front-end techniques. I love structure and order and I also stand for quality. I love spending time on fixing little details and optimizing web apps. Also I like working in a team, you'll learn faster and much more. As the saying goes: 'two heads are better than one'.
+
+</h2>
       </div>
     </div>
   )
